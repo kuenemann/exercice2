970b53db3a91613a01fdd1a7971dfe89050a38b8
kuenemann <leokuenemann2@gmail.com>
2023-03-08 14:47:01 +0100

    exercice2

---
+++ demo.ts
@@ -0,0 +1,90 @@
+console.log("hello")
+// Exercice # 0
+
+// À la racine du répertoire de ce projet, supprimer le dossier .git, vous pouvez exécuter la commande "git status" pour vérifier si git est initialiser ou non.
+// Toujours à la racine du répertoire de ce projet, initialiser git et faites les commandes nécessaires pour pousser ce projet dans un dépot Github.
+// Partager l'adresse de ce nouveau repo, contenant ce projet, au formateur.
+
+// Exercice # 1
+
+// - Créer une fonction Javascript avec deux constantes nommées : "address" et "zipCode", la première est une string, la seconde est un nombre, veillez à bien les typer.
+// - Créer une constante nommée "addressFull" contenant la concaténation de ces deux variables, veillez à bien les typer.
+// - Faire s'afficher la constante "addressFull" dans la console du navigateur.
+ const address:string="120chemindemoncul"
+ const ZipCode:number="0796059609"
+ function exercice1{
+    console.log("string+''+number");
+ }
+ exercice1();
+// déclarer sa variable
+    
+
+
+// - Créer une fonction Javascript avec une constante contenant un tableau de vos 5 pays favoris, veillez à bien le typer.
+// - À l'aide d'une boucle, itérrer dans ce tableau et afficher chaque valeur dans la console du navigateur.
+
+// Exercice # 3
+
+// - Créer une fonction Javascript pour décomposer une string, exemple : ma phrase : "developpement", resultat souhaité : [d, e, v, e, l, o, p, p, e, m, e, n, t]
+// - Le resultat est stocké dans une constante correctement typée dont vous afficherez la valeur dans la console du navigateur.
+
+// Exercice # 4
+
+// - Créer une fonction Javascript pour recomposer une string, exemple : ma phrase décomposée : [d, e, v, e, l, o, p, p, e, m, e, n, t], résultat souhaité : "developpement"
+// - Le resultat est stockée dans une constante correctement typée dont vous afficherez la valeur dans la console du navigateur.
+
+
+
+
+
+// Exercice # 1
+
+// - Écrivez la suite d'actions à réaliser pour vérifier si le ticket de loterie que le client vous tend est gagnant.
+// - Si le chiffre est compris entre 1 et 5 annoncer au client qu'il a gagné, autrement indiquer lui qu'il à perdu et invitez le à rejouer.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+dire bonjour au client    
+prendre le ticket du client
+le passer dans la machine,verifier si il est gagnant 
+lui dire qu'il a perdu et lui proposer de rejouer 
+
+
+// Exerice # 2
+
+// - Écrivez la suite d'actions à réaliser pour connaitre le montant des courses du clients TTC.
+// - Votre caisse enregistreuse est cassées, vous devez aditionner les prix HT de chaque produit du client et y ajouter la TVA de 20%.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+dire bonjour au client     
+passer tous les acrticles
+calculer le prix hors taxes le noter 
+rajouter 20% de tva 
+et lui annoncer la bonne nouvelle du paiement
+
+// Exercice # 3
+
+// - Typer les constantes suivantes :
+// const organism = "Simplon"
+// const learners = 15
+// const promotionStarted = true
+// const learnerNames = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"]
+// const pedagogicalReferent = { name: "Tiphaine", work: "Simplon" }
+// const absentLearner = undefined
+   
+// Exercice # 4
+
+// - Créer une fonction Javascript avec une constante nommée "country", cette constante est typée grâce à Typescript et contient le nom d'un pays.
+// - Faire s'afficher le contenu de la constante dans la console du navigateur.
+const country: string="france"
+function exercice 4(){
+    console.log(country);
+}
+// Exercice # 5
+
+// - Créer une fonction Javascript avec deux constantes nommées : "firstName" et "lastName", ces deux constantes sont typées grâce à Typescript et contiennent vos nom / prénom.
+// - Faire s'afficher la concaténation de ces deux variables dans la console du navigateur.
+    const firstName:string="leo"
+    const lastName:string="kuenemann"
+
+    function exercice5{
+        console.log(firstName+""+lastName);
+    }
+    exercice5();
---
+++ realisations/demo.ts
@@ -0,0 +1,38 @@
+// Exercice # 1
+
+// - Écrivez la suite d'actions à réaliser pour vérifier si le ticket de loterie que le client vous tend est gagnant.
+// - Si le chiffre est compris entre 1 et 5 annoncer au client qu'il a gagné, autrement indiquer lui qu'il à perdu et invitez le à rejouer.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+
+    prendre le ticket du client 
+    verifier si il est gagnant
+    le mettre dans la machine 
+    si il est gagnant lui donner ça recompense 
+    et lui proposer si il en veux un autre 
+// Exerice # 2
+
+// - Écrivez la suite d'actions à réaliser pour connaitre le montant des courses du clients TTC.
+// - Votre caisse enregistreuse est cassées, vous devez aditionner les prix HT de chaque produit du client et y ajouter la TVA de 20%.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+    
+    passer les produits
+    compter le nombre darticles 
+    calculer le montant hors tax
+    et y ajouter les 20% de tva 
+
+// Exercice # 3
+
+// - Typer les constantes suivantes :
+// const organism = "Simplon"
+// const learners = 15
+// const promotionStarted = true
+// const learnerNames = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"]
+// const pedagogicalReferent = { name: "Tiphaine", work: "Simplon" }
+// const absentLearner = undefined
+// const excludedLearner = unknow
+
+    const organism organism !string Simplon
+    const organism learners !number *15
+    const organism  promotionStarted
+    const organism  learnerNames 
+  
